eab3ccbb1b1c02bf4fdf3772295c0dc76a5a6d67
Eduardo Augusto Lima Pereira <42621512+Edd002@users.noreply.github.com>
2025-08-17 15:55:29 -0300

    Initial commit

---
+++ src/main/java/com/fiap/tech/challenge/FTCRestaurantHistoryApi.java
@@ -0,0 +1,12 @@
+package com.fiap.tech.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FTCRestaurantHistoryApi {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FTCRestaurantHistoryApi.class, args);
+    }
+}
---
+++ src/test/java/com/fiap/tech/challenge/FTCRestaurantHistoryApiTests.java
@@ -0,0 +1,12 @@
+package com.fiap.tech.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FTCRestaurantHistoryApiTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
