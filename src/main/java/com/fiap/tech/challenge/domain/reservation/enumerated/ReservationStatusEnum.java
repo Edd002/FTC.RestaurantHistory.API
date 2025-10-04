806c9fd1a34b692947f75fbd0b7d2ed406f1bb25
italo <italo-ventura@hotmail.com>
2025-10-04 20:43:36 -0300

    Fase-3: Ajustes no enum de status de reservation e consumer

--- src/main/java/com/fiap/tech/challenge/domain/reservation/enumerated/ReservationStatusEnum.java
+++ src/main/java/com/fiap/tech/challenge/domain/reservation/enumerated/ReservationStatusEnum.java
@@ -3,5 +3,6 @@ package com.fiap.tech.challenge.domain.reservation.enumerated;
 public enum ReservationStatusEnum {
     REQUESTED,
     ACCEPTED,
-    REJECTED
+    REJECTED,
+    CANCELED
 }
--- src/main/java/com/fiap/tech/challenge/messaging/consumer/ReservationConsumer.java
+++ src/main/java/com/fiap/tech/challenge/messaging/consumer/ReservationConsumer.java
@@ -5,7 +5,9 @@ import com.fiap.tech.challenge.domain.reservation.dto.ReservationMessageDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 
@@ -20,6 +22,10 @@ public class ReservationConsumer {
     }
 
     @KafkaListener(topics = "${spring.kafka.topic.name.reservation-topic}", groupId = "${spring.kafka.consumer.group-id}", containerFactory = "reservationKafkaListenerContainerFactory")
+    @RetryableTopic(
+            backoff = @Backoff(value = 3000L),
+            autoCreateTopics = "false",
+            include = Exception.class)
     public void listenToReservationTopic(ConsumerRecord<String, ReservationMessageDTO> message, Acknowledgment ack) {
         try {
             ReservationMessageDTO reservation = message.value();
@@ -27,7 +33,8 @@ public class ReservationConsumer {
             service.receiveReservation(reservation);
             ack.acknowledge();
         } catch (Exception e) {
-            log.error("Erro ao processar o pedido: {}", e.getMessage());
+            log.error("Erro ao processar reserva: {}", e.getMessage());
+            throw e;
         }
     }
 }
